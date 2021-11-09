2777e24ef322ee12cf99cc99d94e62233f731ae6
Jennifer <Rafter.jennifer@gmail.com>
2021-11-09 12:50:55 -0800

    fixed merge conflicts

--- routes/helpRoute.js
+++ routes/helpRoute.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const router = express.Router();
 
@@ -17,23 +16,3 @@ router.get("/contactus", (req, res) => {
     res.send("welcome to the contactus page");
   });
 module.exports = router;
-=======
-const express = require("express");
-const router = express.Router();
-
-
-///http://localhost:3002/help
-router.get("/", (req, res) => {
-  res.send("welcome to the help page");
-});
-///http://localhost:3002/help/policy
-router.get("/policy", (req, res) => {
-    res.send("welcome to the policy page");
-  });
-
-  ///http://localhost:3002/help/contactus
-router.get("/contactus", (req, res) => {
-    res.send("welcome to the contactus page");
-  });
-module.exports = router;
->>>>>>> 847570d87c2be356c35a785e2d80bd80d472c465
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const router = express.Router();
 const { ensureAuthenticated, isAdmin } = require("../middleware/checkAuth");
@@ -16,22 +15,3 @@ router.get("/reminders", ensureAuthenticated, (req, res) => {
 });
 
 module.exports = router;
-=======
-const express = require("express");
-const router = express.Router();
-const { ensureAuthenticated, isAdmin } = require("../middleware/checkAuth");
-//welcome page localhost:3002
-router.get("/", (req, res) => {
-  res.send("welcome");
-});
-
-//if ensureAuthenticated gives permission then the code will run
-//reminders page ("dasboard") localhost:3002/reminders
-router.get("/reminders", ensureAuthenticated, (req, res) => {
-  res.render("reminders", {
-    user: req.user,
-  });
-});
-
-module.exports = router;
->>>>>>> 847570d87c2be356c35a785e2d80bd80d472c465
