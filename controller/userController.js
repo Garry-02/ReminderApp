55ef41c3babdc63fbb2df1766fd84d55f71e1327
Jennifer <Rafter.jennifer@gmail.com>
2021-11-09 12:51:53 -0800

    fixed merge problems

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const userModel = require("../models/userModel").userModel;
 
 const getUserByEmailIdAndPassword = (email, password) => {
@@ -25,32 +24,4 @@ function isUserValid(user, password) {
 module.exports = {
   getUserByEmailIdAndPassword,
   getUserById,
-=======
-const userModel = require("../models/userModel").userModel;
-
-const getUserByEmailIdAndPassword = (email, password) => {
-  let user = userModel.findOne(email);
-  if (user) {
-    if (isUserValid(user, password)) {
-      return user;
-    }
-  }
-  return null;
-};
-const getUserById = (id) => {
-  let user = userModel.findById(id);
-  if (user) {
-    return user;
-  }
-  return null;
-};
-
-function isUserValid(user, password) {
-  return user.password === password;
-}
-
-module.exports = {
-  getUserByEmailIdAndPassword,
-  getUserById,
->>>>>>> 847570d87c2be356c35a785e2d80bd80d472c465
 };
