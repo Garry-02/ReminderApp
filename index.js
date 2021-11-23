236d689e43e7930ffe611129391be05620ec41f3
hontailhd <78238447+hontailhd@users.noreply.github.com>
2021-11-23 06:54:41 -0800

    update in index.js
    need to attach the urls generated by the
    unsplash
    
    and enable useers to import their own pictures.

--- index.js
+++ index.js
@@ -14,6 +14,24 @@ const fs = require("fs");
 
 require("dotenv").config()
 
+const passport = require("./controller/middleware/passport");
+const authRoute = require("./routes/authRoute");
+const indexRoute = require("./routes/indexRoute");
+const adminRoute = require("./routes/adminRoute");
+
+//start of the Profile Pic section
+
+//random pic section
+
+function randPicGen() {
+
+  const rand_pic_url = 'https://api.unsplash.com/photos/random?client_id=SkbZtC5qiRRqDkFJBTl7J9bN8Ar5t5doimLxs-GCvoc'
+
+  fetch(rand_pic_url) 
+}
+// now add this for each profile
+
+
 const storage = multer.diskStorage({
   destination: "./uploads",
   filename: (req, file, callback) => {
@@ -29,6 +47,8 @@ const upload = multer({
 
 
 app.use(cors());
+
+//inserted into ProfilePic section
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
@@ -42,15 +62,16 @@ app.use(
     },
   })
 );
+//ask jennifer about below commented out code
+// why was this not inserted
 
+// app.use(express.json({ extended: false }));
+// app.use(express.urlencoded({ extended: true }));
 
-
-const passport = require("./controller/middleware/passport");
-const authRoute = require("./routes/authRoute");
-const indexRoute = require("./routes/indexRoute");
-const adminRoute = require("./routes/adminRoute");
 
 app.use(upload.any());
+// removes the image stored in "/uploads/" file
+// after uploading the profile pic to imgur.
 app.post("/uploads/", async (req, res) => {
   const file = req.files[0];
   try {
@@ -61,6 +82,7 @@ app.post("/uploads/", async (req, res) => {
     console.log("error", error);
   }
 });
+// End of ProfilePic section.
 
 app.use(express.urlencoded({ extended: true }));
 app.use(expressLayouts);
