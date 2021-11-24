f4d83d038f0d0187c933c4a066b75709e3309d0c
Garry-02 <gurjyotsingh76@gmail.com>
2021-11-24 13:09:39 -0800

    Added username to register

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -16,14 +16,18 @@ let authController = {
 
   registerSubmit: (req, res) => {
     let rand_pic_url = 'https://api.unsplash.com/photos/random?client_id=SkbZtC5qiRRqDkFJBTl7J9bN8Ar5t5doimLxs-GCvoc'
-  fetch(rand_pic_url).then(data => data.json()).then(d => {
+    fetch(rand_pic_url).then(data => data.json()).then(d => {
     let newUser = {
-      id: Math.floor(Math.random * 1000) + 1,
+      //id: Math.floor(Math.random * 1000) + 1,
+      id: 20,
+      name: req.body.username,
       email: req.body.email,
       password: req.body.password,
       reminders: [],
-      profilePic: d.urls.full
+      role: "user",
+      profilePic: d.urls.full,
     }
+    console.log(newUser);
     database.push(newUser);
     res.redirect("/login") // somewhere
 
