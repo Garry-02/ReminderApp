18b3d5f233daee54b55b53b77559b20b01982d74
Jennifer <Rafter.jennifer@gmail.com>
2021-12-03 18:13:25 -0800

    deleted commented out code that is unused

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -3,7 +3,9 @@ const fetch = require("node-fetch")
 //let ID = {
  // getRandom() {
   //return Math.random();
-//}};
+//}}; Tried to create a function to use this as an id generator but feel that 
+//this was not working becuase in the database we have an id hardcoded, I tried to 
+// add a function in the database where Id is but it also nroke the code as it does below.
 
 let authController = {
   login: (req, res) => {
@@ -24,7 +26,7 @@ let authController = {
     let rand_pic_url = 'https://api.unsplash.com/photos/random?client_id=SkbZtC5qiRRqDkFJBTl7J9bN8Ar5t5doimLxs-GCvoc'
     fetch(rand_pic_url).then(data => data.json()).then(d => {
     let newUser = {
-      //id: Math.floor(Math.random * 1000) + 1,
+      //id: Math.floor(Math.random * 1000) + 1, this broke the code everytime no matter what I tried, so we had to assign a value
       id: 5,   
       name: req.body.username,
       email: req.body.email,
@@ -35,7 +37,7 @@ let authController = {
     }
     console.log(newUser);
     database.push(newUser);
-    res.redirect("/login") // somewhere
+    res.redirect("/login") 
 
   })
   },
