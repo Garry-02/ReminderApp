4897059622b809c8c75b5d0208951ae7347f80fb
Jennifer <Rafter.jennifer@gmail.com>
2021-12-03 18:15:43 -0800

    deleted unused code that was moved to other files

--- public/app.js
+++ public/app.js
@@ -1,18 +1,3 @@
-// const express = require("express");
-// const expressLayouts = require("express-ejs-layouts");
-// const { ensureAuthenticated} = require("./controller/middleware/checkAuth")
-// const app = express();
-// const path = require("path");
-// const ejsLayouts = require("express-ejs-layouts");
-// const reminderController = require("./controller/reminder_controller");
-// const authController = require("./controller/auth_controller");
-// const session = require("express-session");
-// const multer = require("multer");
-// const imgur = require("imgur");
-// const cors = require("cors");
-// const fs = require("fs");
-// let {database} = require("../models/userModel");
-
 
 document.getElementById("file").addEventListener("change", (ev) => {
     ev.preventDefault();
@@ -28,53 +13,3 @@ document.getElementById("file").addEventListener("change", (ev) => {
   });
 
 
-//inserted into ProfilePic section
-/*
-app.use(express.static(path.join(__dirname, "public")));
-app.use(
-  session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      secure: false,
-      maxAge: 24 * 60 * 60 * 1000,
-    },
-  })
-);
-//ask jennifer about below commented out code
-// why was this not inserted
-
-// app.use(express.json({ extended: false }));
-// app.use(express.urlencoded({ extended: true }));
-
-
-app.use(upload.any());
-// removes the image stored in "/uploads/" file
-// after uploading the profile pic to imgur.
-app.post("/uploads/", async (req, res) => {
-  const file = req.files[0];
-  try {
-    const url = await imgur.uploadFile(`./uploads/${file.filename}`);
-    res.json({ message: url.data.link });
-    
-    
-    let userPic = url.data.link
-    console.log(userPic)
-    database.get(user.profilePic)
-    console.log(user.profilePic)  
-    //target the "id" element of the current user
-    
-  
-    // get "profilePic" from "database", from usermodel
-    //change the profilePic to url.data.link
-
-
-    fs.unlinkSync(`./uploads/${file.filename}`);
-  } catch (error) {
-    console.log("error", error);
-  }
-});
-
-*/
